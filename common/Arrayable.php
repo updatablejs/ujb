5b5e9910f080bce7ba741b5545fee0b92cfa4188
updatablejs <contact@updatablejs.com>
2024-04-28 13:59:05 +0300

    deleted redundant files

--- common/Arrayable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace core\common;
-
-interface Arrayable {
-	
-	public function toArray();
-}
--- database/sql/components/Where.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace core\database\sql\components;
-
-class Where extends Conditions {}
